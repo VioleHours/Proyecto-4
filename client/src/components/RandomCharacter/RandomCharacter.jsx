a2c61834d3aec0974e4a89903bacdba77e0857ff
Viole Hours <holasoyviole@gmail.com>
2023-10-01 23:33:17 -0300

    random

--- client/src/components/RandomCharacter/RandomCharacter.jsx
+++ client/src/components/RandomCharacter/RandomCharacter.jsx
@@ -1,3 +1,7 @@
-export default function RandomCharacter(){
-    
+export default function RandomCharacter(props) {
+  return (
+    <div>
+      <button onClick={props.addRandomCharacter}>PIRURIRU PIRURIRURI</button>
+    </div>
+  );
 }
